9ea668fe32da5ca1738dba9eb87ecf6634dd6e6e
RadchenkoMukola <RadchenkoMukola@gmail.com>
2025-11-27 10:48:02 +0200

    updated service+main+routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,21 @@
-from fastapi import FastAPI
-from services.graph_service import get_all_nodes, get_books
-from services.pokeapi_service import get_pokemon
+from fastapi import APIRouter
+from app.services.graph_service import get_all_nodes, get_books
+from app.services.pokeapi_service import get_pokemon
 
-app = FastAPI(
-    title="Pokémon Multi-Model API",
-    description="Service Layer for Neo4j + PokeAPI + (future Oracle Spatial)",
-    version="1.0"
-)
+router = APIRouter()
 
-@app.get("/")
+@router.get("/")
 def root():
     return {"message": "Pokémon Multi-Model API is running!"}
 
-@app.get("/nodes")
+@router.get("/nodes")
 def read_nodes(limit: int = 10):
     return get_all_nodes(limit)
 
-@app.get("/books")
+@router.get("/books")
 def read_books():
     return get_books()
 
-@app.get("/pokemon/{name}")
+@router.get("/pokemon/{name}")
 def read_pokemon(name: str):
     return get_pokemon(name)
